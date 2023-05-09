7ab09fd4168fad4c637b947900e5cb02299d5b88
baojingh@163.com <baojingh@163.com>
2023-05-09 23:23:59 +0800

    update: add dep

---
+++ main.go
@@ -0,0 +1,28 @@
+package mdocker
+
+/**
+  @Author   : bob
+  @Datetime : 2023-05-09 下午 11:08
+  @File     : main.go
+  @Desc     :
+*/
+
+import (
+	"github.com/docker/docker/client"
+	log "github.com/sirupsen/logrus"
+)
+
+func init() {
+	log.SetFormatter(&log.TextFormatter{
+		TimestampFormat:           "2006-01-02 15:04:05",
+		ForceColors:               true,
+		EnvironmentOverrideColors: true,
+		FullTimestamp:             true,
+		DisableLevelTruncation:    true,
+	})
+}
+
+func main() {
+	log.Info("hello")
+
+}
