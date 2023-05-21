9eefc28a98f87320839d8e29fe35ef7559acfab9
baojingh@163.com <baojingh@163.com>
2023-05-21 20:32:30 +0800

    update: config with viper

--- ws/ws_client.go
+++ ws/ws_client.go
@@ -1,6 +1,7 @@
 package ws_handle
 
 import (
+	"errors"
 	"github.com/gorilla/websocket"
 	log "mdocker/logger"
 	"net/http"
@@ -13,20 +14,55 @@ import (
   @Desc     :
 */
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-	CheckOrigin: func(r *http.Request) bool {
-		return true
-	},
+type WsClient struct {
+	conn           *websocket.Conn
+	name           string
+	active         bool
+	receiveMsgChan chan []byte
+	sendMsgChan    chan []byte
 }
 
-func getWs(w http.ResponseWriter, r *http.Request) (*websocket.Conn, error) {
+var wsClientsMap = make(map[*WsClient]bool)
+
+func RegisterWsClient(w http.ResponseWriter, r *http.Request, name string) (*WsClient, error) {
+	upgrader := websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Log.Error("fail to create ws success, ", err)
 		return nil, err
 	}
-	log.Log.Infof("get ws success")
-	return conn, nil
+	log.Log.Infof("get ws client name %s", name)
+	client := &WsClient{
+		conn:           conn,
+		name:           name,
+		active:         true,
+		receiveMsgChan: make(chan []byte, 10),
+		sendMsgChan:    make(chan []byte, 10),
+	}
+	wsClientsMap[client] = true
+	return client, nil
+}
+
+func UnregisterClient(client *WsClient) (bool, error) {
+	if client == nil {
+		log.Log.Error("the client that request to unregister is nil")
+		return false, errors.New("NilErr")
+	}
+	if _, isExit := wsClientsMap[client]; isExit != true {
+		delete(wsClientsMap, client)
+		log.Log.Infof("delete client %s success", client)
+		return true, nil
+	}
+	log.Log.Infof("fail to delete client %s ", client)
+	return false, errors.New("NotFoundErr")
+}
+
+func GetClients() map[*WsClient]bool {
+	return wsClientsMap
 }
--- ws/ws_handler.go
+++ ws/ws_handler.go
@@ -1,7 +1,6 @@
 package ws_handle
 
 import (
-	"encoding/json"
 	"github.com/docker/docker/api/types"
 	"github.com/gorilla/websocket"
 	"io"
@@ -9,7 +8,6 @@ import (
 	"mdocker/container"
 	log "mdocker/logger"
 	"net/http"
-	"time"
 	"unicode/utf8"
 )
 
@@ -27,157 +25,156 @@ type clientStruct struct {
 	statsChan chan types.StatsJSON
 }
 
-func ContainerLogs(w http.ResponseWriter, r *http.Request) {
-	conn, _ := getWs(w, r)
-	cli := &clientStruct{
-		conn:     conn,
-		sendChan: make(chan []byte, 100),
-	}
-	defer func() {
-		log.Log.Info("cli.conn is closed")
-		cli.conn.Close()
-	}()
-
-	go func() {
-		// 传递参数
-		// ws://127.0.0.1:8081/logs?id=a315b7da073d
-		containerId := r.URL.Query().Get("id")
-		for {
-			reader, err := container.ContainerLogs(containerId)
-			if err != nil {
-				log.Log.Error("fail to get the container logs reader, ", err)
-				return
-			}
-			defer func() {
-				log.Log.Info("reader is closed")
-				reader.Close()
-			}()
-			cli.reader = reader
-			err = ReceiveFromDocker(cli)
-			if err != nil {
-				log.Log.Error("fail to read container logs, ", err)
-				// TODO 当容器重启之后，reader会被关闭。在实际场景中需要重新获取reader
-				// 此处待优化。临时方案是重新获取reader
-				reader, err = container.ContainerLogs(containerId)
-			}
-		}
-	}()
-	go func() {
-		ReceiveFromClient(cli)
-	}()
-	SendFromServer(cli)
-}
-
-func ContainerStats(w http.ResponseWriter, r *http.Request) {
-	conn, _ := getWs(w, r)
-	cli := &clientStruct{
-		conn:      conn,
-		sendChan:  make(chan []byte, 100),
-		statsChan: make(chan types.StatsJSON, 100),
-	}
-	defer func() {
-		log.Log.Info("cli.conn is closed")
-		cli.conn.Close()
-	}()
-
-	// 传递参数
-	// ws://127.0.0.1:8081/stats?id=a315b7da073d
-	containerId := r.URL.Query().Get("id")
-
-	reader, err := container.ContainerStats(containerId)
-	if err != nil {
-		log.Log.Error("fail to get the container stats reader, ", err)
-		return
-	}
-	defer func() {
-		log.Log.Info("stats reader is closed")
-		reader.Close()
-	}()
-	go func() {
-		var statsValue types.StatsJSON
-		for true {
-			decoder := json.NewDecoder(reader)
-			err := decoder.Decode(&statsValue)
-			if err != nil {
-				log.Log.Error("cannot decode stats data, ", err)
-			}
-			cli.statsChan <- statsValue
-			if err != nil {
-				log.Log.Error("fail to read container logs, ", err)
-				// TODO 当容器重启之后，reader会被关闭。在实际场景中需要重新获取reader
-				// 此处待优化。临时方案是重新获取reader
-				reader, err = container.ContainerStats(containerId)
-			}
-		}
-		time.Sleep(time.Second * 1)
-	}()
-
-	go func() {
-		ReceiveFromClient(cli)
-	}()
-	for stats := range cli.statsChan {
-		byteArr, err := convertStatsJSONToByte(stats)
-		err = cli.conn.WriteMessage(websocket.TextMessage, byteArr)
-		if err != nil {
-			log.Log.Error("fal to send data to client, ", err)
-			break
-		}
-		log.Log.Info(stats)
-	}
-}
-
-func convertStatsJSONToByte(stats types.StatsJSON) ([]byte, error) {
-	// 使用 json.Marshal() 将 StatsJSON 编码为 JSON 字节数组
-	bytes, err := json.Marshal(stats)
-	if err != nil {
-		return nil, err
-	}
-	return bytes, nil
-}
-
-func ContainerInspect(w http.ResponseWriter, r *http.Request) {
-	// 传递参数
-	// ws://127.0.0.1:8081/inspect?id=a315b7da073d
-	containerId := r.URL.Query().Get("id")
-	inspect, err := container.ContainerInspect(containerId)
-	if err != nil {
-		log.Log.Error("fail to get the container inspect, ", err)
-		return
-	}
-	jsonString, err := json.Marshal(inspect)
-	w.Write(jsonString)
-}
+//
+//func ContainerLogs(w http.ResponseWriter, r *http.Request) {
+//	conn, _ := RegisterWsClient(w, r)
+//	cli := &clientStruct{
+//		conn:     conn,
+//		sendChan: make(chan []byte, 100),
+//	}
+//	defer func() {
+//		log.Log.Info("cli.conn is closed")
+//		cli.conn.Close()
+//	}()
+//
+//	go func() {
+//		// 传递参数
+//		// ws://127.0.0.1:8081/logs?id=a315b7da073d
+//		containerId := r.URL.Query().Get("id")
+//		for {
+//			reader, err := container.ContainerLogs(containerId)
+//			if err != nil {
+//				log.Log.Error("fail to get the container logs reader, ", err)
+//				return
+//			}
+//			defer func() {
+//				log.Log.Info("reader is closed")
+//				reader.Close()
+//			}()
+//			cli.reader = reader
+//			err = ReceiveFromDocker(cli)
+//			if err != nil {
+//				log.Log.Error("fail to read container logs, ", err)
+//				// TODO 当容器重启之后，reader会被关闭。在实际场景中需要重新获取reader
+//				// 此处待优化。临时方案是重新获取reader
+//				reader, err = container.ContainerLogs(containerId)
+//			}
+//		}
+//	}()
+//	go func() {
+//		ReceiveFromClient(cli)
+//	}()
+//	SendFromServer(cli)
+//}
+//
+//func ContainerStats(w http.ResponseWriter, r *http.Request) {
+//	conn, _ := RegisterWsClient(w, r)
+//	cli := &clientStruct{
+//		conn:      conn,
+//		sendChan:  make(chan []byte, 100),
+//		statsChan: make(chan types.StatsJSON, 100),
+//	}
+//	defer func() {
+//		log.Log.Info("cli.conn is closed")
+//		cli.conn.Close()
+//	}()
+//
+//	// 传递参数
+//	// ws://127.0.0.1:8081/stats?id=a315b7da073d
+//	containerId := r.URL.Query().Get("id")
+//
+//	reader, err := container.ContainerStats(containerId)
+//	if err != nil {
+//		log.Log.Error("fail to get the container stats reader, ", err)
+//		return
+//	}
+//	defer func() {
+//		log.Log.Info("stats reader is closed")
+//		reader.Close()
+//	}()
+//	go func() {
+//		var statsValue types.StatsJSON
+//		for true {
+//			decoder := json.NewDecoder(reader)
+//			err := decoder.Decode(&statsValue)
+//			if err != nil {
+//				log.Log.Error("cannot decode stats data, ", err)
+//			}
+//			cli.statsChan <- statsValue
+//			if err != nil {
+//				log.Log.Error("fail to read container logs, ", err)
+//				// TODO 当容器重启之后，reader会被关闭。在实际场景中需要重新获取reader
+//				// 此处待优化。临时方案是重新获取reader
+//				reader, err = container.ContainerStats(containerId)
+//			}
+//		}
+//		time.Sleep(time.Second * 1)
+//	}()
+//
+//	go func() {
+//		ReceiveFromClient(cli)
+//	}()
+//	for stats := range cli.statsChan {
+//		byteArr, err := convertStatsJSONToByte(stats)
+//		err = cli.conn.WriteMessage(websocket.TextMessage, byteArr)
+//		if err != nil {
+//			log.Log.Error("fal to send data to client, ", err)
+//			break
+//		}
+//		log.Log.Info(stats)
+//	}
+//}
+//
+//func convertStatsJSONToByte(stats types.StatsJSON) ([]byte, error) {
+//	// 使用 json.Marshal() 将 StatsJSON 编码为 JSON 字节数组
+//	bytes, err := json.Marshal(stats)
+//	if err != nil {
+//		return nil, err
+//	}
+//	return bytes, nil
+//}
+//
+//func ContainerInspect(w http.ResponseWriter, r *http.Request) {
+//	// 传递参数
+//	// ws://127.0.0.1:8081/inspect?id=a315b7da073d
+//	containerId := r.URL.Query().Get("id")
+//	inspect, err := container.ContainerInspect(containerId)
+//	if err != nil {
+//		log.Log.Error("fail to get the container inspect, ", err)
+//		return
+//	}
+//	jsonString, err := json.Marshal(inspect)
+//	w.Write(jsonString)
+//}
 
 func ContainerExec(w http.ResponseWriter, r *http.Request) {
-	conn, _ := getWs(w, r)
-	cli := &clientStruct{
-		conn:      conn,
-		sendChan:  make(chan []byte, 100),
-		statsChan: make(chan types.StatsJSON, 100),
-	}
-	defer func() {
-		log.Log.Info("cli.conn is closed")
-		cli.conn.Close()
-	}()
-	r.ParseForm()
 	// 传递参数
 	// ws://127.0.0.1:8081/exec?id=a315b7da073d
 	containerId := r.URL.Query().Get("id")
 	log.Log.Infof("get containerid %s", containerId)
-	hr, err := container.ContainerExec(containerId)
+
+	clientName := r.URL.Query().Get("name")
+	log.Log.Infof("get containws client name %s", clientName)
+	wsClient, _ := RegisterWsClient(w, r, clientName)
+	defer func() {
+		log.Log.Info("cli.conn is closed")
+		wsClient.conn.Close()
+	}()
+
+	containerHr, err := container.ContainerExec(containerId)
 	if err != nil {
 		log.Log.Error("fail to get the container exec, ", err)
 		return
 	}
 
-	// 转发输入/输出至websocket
+	// 获取容器返回的消息，并将其转发到websocket
 	go func() {
 		buf := make([]byte, 512)
 		for {
-			nr, err := hr.Conn.Read(buf)
+			nr, err := containerHr.Conn.Read(buf)
 			log.Log.Infof("read data from container, %s", string(buf))
 			if nr > 0 {
-				err := conn.WriteMessage(websocket.TextMessage, buf[0:nr])
+				err := wsClient.conn.WriteMessage(websocket.TextMessage, buf[0:nr])
 				if err != nil {
 					return
 				}
@@ -187,13 +184,14 @@ func ContainerExec(w http.ResponseWriter, r *http.Request) {
 			}
 		}
 	}()
+	// 读取界面的输入数据，并将其转发到容器中
 	for {
-		_, message, err := cli.conn.ReadMessage()
+		_, message, err := wsClient.conn.ReadMessage()
 		if !utf8.Valid(message) {
 			log.Log.Errorf("Received message from client contains invalid UTF-8: %v", message)
 			continue
 		}
-		hr.Conn.Write(message)
+		containerHr.Conn.Write(message)
 		log.Log.Infof("read message from client, %s", string(message))
 		if err != nil {
 			if websocket.IsCloseError(err, websocket.CloseGoingAway, websocket.CloseNormalClosure) {
